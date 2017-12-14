7df57d9cc5cdb431d0df69d10441d07ddb2f10cf
JJ Guaragno <just2jays@gmail.com>
2017-12-14 17:12:47 -0500

    try this out

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var racing = require('./racing');
+var ceelo = require('./ceelo');
+
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.set('port', port);
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, function(){
-    console.log("Server started ..!");
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
 });
+
+app.listen(port, function () {
+  console.log('Server running on port: ' + port);
+});
+
+app.post('/racing', racing);
+app.post('/ceelo', ceelo);
