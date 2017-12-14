b64368e25b5506ad4069efaa66b09a25d3beb99e
JJ Guaragno <just2jays@gmail.com>
2017-12-14 17:25:23 -0500

    tryer out

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.set('port', port);
 
-app.listen(port, function(){
-    console.log("Server started ..!");
-});
+app.use(express.static(path.join(__dirname, 'racing')));
 
-app.get('/', function (req, res) {
-    res.send('root')
-})
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
