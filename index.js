c397e6d7225d8899fb549c4985b76c42e5f5ecb3
JJ Guaragno <just2jays@gmail.com>
2017-12-14 17:08:13 -0500

    no need i hope

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var port = process.env.PORT || 3000;
 
 app.set('port', port);
 
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.listen(port, function(){
     console.log("Server started ..!");
 });
