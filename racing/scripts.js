0eceea43090352f88495282ede8d1e2262ecd429
JJ Guaragno <just2jays@gmail.com>
2017-12-14 15:27:08 -0500

    winner declaration for horses

--- racing/scripts.js
+++ racing/scripts.js
@@ -1,12 +1,26 @@
 var raceLength = window.outerWidth*0.75;
+var winnerChosen = false;
 
 $(document).ready(function(){
-$('#beginRace').on('click', function(){
-    setInterval(function(){
-    beginRace();
-  }, 500);
+  $('#beginRace').on('click', function(){
+      setInterval(function(){
+      beginRace();
+    }, 500);
+  });
+
+  $('#newHorse').on('submit', function(event){
+    event.preventDefault();
+    var horseInfo = $( this ).serializeArray();
+    addHorse(horseInfo);
+  });
 });
-});
+
+function addHorse(info) {
+  console.log(info);
+  $('.track-container').append('<div class="individual-track"><div class="horse" data-distance=0><span class="horse-label">'+info[0].value+'</span></div></div>');
+  
+  return false;
+}
 
 function beginRace() {
   _.each($('.horse'), function(horse) {
@@ -14,8 +28,13 @@ function beginRace() {
      var currentDistance = $(horse).data( "distance" );
     var newDistance = currentDistance + addDistance;
     if(newDistance >= raceLength){
+      if(!winnerChosen) {
+        $(horse).parent().append('<div class="horse-rank">Winner!!!</div>');
+        console.log('We have a winner!');
+        winnerChosen = true;
+      }
       $(horse).remove();
-      console.log('We have a winner!');
+      
     }else{
       $(horse).data('distance', newDistance); 
       $(horse).animate({
