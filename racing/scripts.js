9131a9bb144d5c3224b4c53d86108f762dd0673d
JJ Guaragno <just2jays@gmail.com>
2018-01-12 15:34:51 -0500

    add real horse pics

--- racing/scripts.js
+++ racing/scripts.js
@@ -1,11 +1,12 @@
-var raceLength = window.outerWidth*0.75;
+var raceLength = 1000;
 var winnerChosen = false;
 
 $(document).ready(function(){
+  //console.log(raceLength);
   $('#beginRace').on('click', function(){
       setInterval(function(){
       beginRace();
-    }, 200);
+    }, 500);
   });
 
   $('#newHorse').on('submit', function(event){
@@ -16,21 +17,25 @@ $(document).ready(function(){
 });
 
 function addHorse(info) {
-  console.log(info);
-  $('.track-container').append('<div class="individual-track"><div class="horse" data-distance=0><span class="horse-label">'+info[0].value+'</span></div></div>');
+  // console.log('Horse: "' + info[0].value + '" is at the gate!');
+  var horseArray = $('.horse');
+  var horseNumber = _.size(horseArray) + 1;
+
+  $('.track-container').append('<div class="individual-track"><div class="horse" data-horse-number="'+horseNumber+'" data-distance=0><span class="horse-label">'+info[0].value+'</span></div></div>');
   
   return false;
 }
 
 function beginRace() {
   _.each($('.horse'), function(horse) {
-     var addDistance = getRandomArbitrary(1,10);
-     var currentDistance = $(horse).data( "distance" );
+    var addDistance = getRandomArbitrary(10,20);
+    var currentDistance = $(horse).data( "distance" );
     var newDistance = currentDistance + addDistance;
+    //console.log("number: "+$(horse).data( "horse-number" ), "distance: "+currentDistance);
     if(newDistance >= raceLength){
       if(!winnerChosen) {
         $(horse).parent().append('<div class="horse-rank">Winner!!!</div>');
-        console.log('We have a winner!');
+        //console.log('We have a winner!');
         winnerChosen = true;
       }
       $(horse).remove();
